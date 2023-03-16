34251dd4262dec29eb638bb7653f86cd9aaa9336
Marion <mrobert148@gmail.com>
2023-03-16 18:26:30 +0100

    add more sleep after sign_in to give time to pass the capchat verification manually

--- main.py
+++ main.py
@@ -32,19 +32,53 @@ button_sign_in.click()
 time.sleep(3)
 username_input = driver.find_element(By.ID, "username")
 username_input.send_keys(LINKEDIN_USERNAME)
+time.sleep(5)
 password_input = driver.find_element(By.ID, "password")
 password_input.send_keys(LINKEDIN_PASSWORD)
+time.sleep(5)
 button_submit = driver.find_element(By.CSS_SELECTOR, "form.login__form button")
 button_submit.click()
 
 # enter the job i'm looking for
-time.sleep(5)
-job_input = driver.find_element(By.CSS_SELECTOR, "div#keyword-typeahead-instance-ember23 input")
+time.sleep(20)
+job_input = driver.find_element(By.CSS_SELECTOR, "div.jobs-search-box__input--keyword input")
 job_input.send_keys("Développeur junior")
 
 # enter the area for the search
-location_input = driver.find_element(By.CSS_SELECTOR, "div#location-typeahead-instance-ember23 input")
-location_input.send_keys("Ile-de-France")
+time.sleep(3)
+location_input = driver.find_element(By.CSS_SELECTOR, "div.jobs-search-box__input.jobs-search-box__input--location input")
+location_input.send_keys("Ile-de-France, France")
 time.sleep(3)
 location_input.send_keys(Keys.ENTER)
 
+# # click on button "see all"
+# time.sleep(5)
+# see_all_button = driver.find_element(By.CSS_SELECTOR, "div.jobs-job-board-list__footer a")
+# see_all_button.click()
+
+# # Scrolls down job list pane to load all listings and then returns to the top
+# job_list_scroll = driver.find_element_by_class_name("jobs-search-results-list")
+# job_list_scroll.click()
+# html = driver.find_element_by_tag_name("html")
+# html.send_keys(Keys.END)
+# time.sleep(3)
+# html.send_keys(Keys.HOME)
+
+# get all the jobs offers in a list
+time.sleep(5)
+all_job_offers = driver.find_elements(By.CSS_SELECTOR, "ul.scaffold-layout__list-container li a")
+print(len(all_job_offers))
+all_link_offers = [job.get_attribute("href") for job in all_job_offers]
+print(all_link_offers)
+
+for link in all_link_offers:
+    time.sleep(3)
+    driver.get(link)
+    time.sleep(5)
+    apply_button = driver.find_element(By.CSS_SELECTOR, "span.artdeco-button__text")
+    time.sleep(5)
+    if apply_button.get_attribute("innerText") == "Candidature simplifiée":
+        apply_button.click()
+    else:
+        print("it's not a simplified application")
+    index += 1
