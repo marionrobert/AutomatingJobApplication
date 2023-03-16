6b89d7741f98dcb9813af6bd746e8df4820a4742
Marion <mrobert148@gmail.com>
2023-03-16 11:38:30 +0100

    click on sign in anchor tag

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 LINKEDIN_JOBS_SEARCH_URL = "https://www.linkedin.com/jobs/"
 
@@ -16,4 +17,8 @@ buttons_cookies = driver.find_elements(By.CSS_SELECTOR, ".artdeco-global-alert-a
 button_accept_cookies = buttons_cookies[0]
 button_accept_cookies.click()
 
+# click on sign in
+buttons_sign = driver.find_elements(By.CSS_SELECTOR, ".nav__cta-container a")
+button_sign_in = buttons_sign[1]
+button_sign_in.click()
 
