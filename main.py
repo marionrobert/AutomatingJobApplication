7913b1625fb735c643c28a023fcd9a5a84edbe41
Marion <mrobert148@gmail.com>
2023-03-16 11:52:10 +0100

    connect to mmy linkedin account

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import time
+import os
 
 LINKEDIN_JOBS_SEARCH_URL = "https://www.linkedin.com/jobs/"
+LINKEDIN_PASSWORD = os.environ["LINKEDIN_PASSWORD"]
+LINKEDIN_USERNAME = os.environ["LINKEDIN_USERNAME"]
 
 chrome_driver_path = r"C:\Users\Utilisateur1\Development\chromedriver.exe"
 service = Service(chrome_driver_path)
@@ -21,4 +23,12 @@ button_accept_cookies.click()
 buttons_sign = driver.find_elements(By.CSS_SELECTOR, ".nav__cta-container a")
 button_sign_in = buttons_sign[1]
 button_sign_in.click()
+
+# enter my sign_in data
+username_input = driver.find_element(By.ID, "username")
+username_input.send_keys(LINKEDIN_USERNAME)
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys(LINKEDIN_PASSWORD)
+button_submit = driver.find_element(By.CSS_SELECTOR, "form.login__form button")
+button_submit.click()
 
